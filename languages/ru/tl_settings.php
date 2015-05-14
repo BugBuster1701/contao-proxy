318166f9bae20024c4aed084dd3909eb4dcd5ee7
Glen Langer <contao.modul@glen-langer.de>
2015-05-14 14:44:01 +0200

    Update Transifex Language Files (ru)

---
+++ languages/ru/tl_settings.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://docs.transifex.com/faq/#translating
+ * @link https://www.transifex.com/projects/p/contao-proxy/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-03-25T18:26:42+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_proxy']['error_local']         = 'Недопустимый аргумент "%s" в исключениях прокси-сервера.';
+$GLOBALS['TL_LANG']['tl_proxy']['error_scheme']        = 'Схема "%s" не поддерживается.';
+$GLOBALS['TL_LANG']['tl_proxy']['error_url']           = 'Недопустимый аргумент в URL-адресе "%s".';
+$GLOBALS['TL_LANG']['tl_settings']['proxy_local']['0'] = 'Не использовать прокси-сервер для';
+$GLOBALS['TL_LANG']['tl_settings']['proxy_local']['1'] = 'Например: "localhost, 127.0.0.1, .example.com, 192.168." ...';
+$GLOBALS['TL_LANG']['tl_settings']['proxy_out_legend'] = 'Конфигурация прокси-сервера (исходящие соединения)';
+$GLOBALS['TL_LANG']['tl_settings']['proxy_url']['0']   = 'URL-адрес прокси-сервера';
+$GLOBALS['TL_LANG']['tl_settings']['proxy_url']['1']   = 'Например: "http://[user:password@]host[:port]" ...';
+$GLOBALS['TL_LANG']['tl_settings']['useProxy']['0']    = 'Использовать прокси-сервер для доступа к Интернет';
+$GLOBALS['TL_LANG']['tl_settings']['useProxy']['1']    = 'Если не прямое подключение к Интернету возможно,  вы можете указать прокси-сервер.';
+
